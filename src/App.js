6d867f730cca38f11e3b37d40c18baf1698fea07
Arthur Medeiros <medeirosartur48@gmail.com>
2023-09-08 23:07:20 -0300

    upando eu espero

---
+++ src/App.js
@@ -0,0 +1,70 @@
+// Importando
+
+import './App.css';
+import { useState } from 'react';
+import Axios from "axios"
+
+//Funcão Principal
+function App() {
+  //Declarando o nome do Pokemon como variável
+  const [pokeNome, setPokeNome] = useState("")
+  const [pokeChosen, setPokemonChosen] = useState(false)
+  const [pokemon, setPokemon] = useState({
+    name: "", 
+    img: "",
+    id: "", 
+    hp: "",
+    attack: "",
+    defense: "",
+    type: "",
+  });
+  //Puxando com o Axios o pokemon com a URL da API
+  const procuraPoke = () => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeNome}`).then((response) => {
+      //Pegando todas as informações de um pokemon
+      setPokemon({
+        name: pokeNome, 
+        img: response.data.sprites.front_default, 
+        id: response.data.id,
+        hp: response.data.stats[0].base_stat,
+        attack: response.data.stats[1].base_stat,
+        defense: response.data.stats[2].base_stat,
+        type: response.data.types[0].type.name,
+      });
+      setPokemonChosen(true)
+    }
+  );
+};
+
+ //O que vai aparecer realmente na página:
+  return (
+    <div className="App">
+      <div className='TitleSection'>
+          <h1>Pokémon DataBase</h1>
+          <input  type="text" 
+                  onChange={(event)=>{setPokeNome(event.target.value)}}
+          />
+          <button onClick={procuraPoke}>Procurar Pokémon</button>
+      </div>
+      <div className='DisplayPoke'>
+        {!pokeChosen ? (
+        <h1>Escolha um Pokémon!</h1>
+        ) : (
+          <>
+            <h1>{pokemon.name}</h1>
+            <img src={pokemon.img}/>
+            <h3>Tipo: {pokemon.type}</h3>
+            <h3>Nº Pokedex: {pokemon.id}</h3>
+            <h4>Hp: {pokemon.hp}</h4>
+            <h4>Ataque: {pokemon.attack}</h4>
+            <h4>Defesa: {pokemon.defense}</h4>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+
+//Exportando
+export default App;
